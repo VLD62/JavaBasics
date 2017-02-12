ec2c35f670b9107e8062db028de2992f25a08763
VLD62 <iliev62@gmail.com>
2017-02-12 14:29:27 +0200

    Adding excersices from Chapter 2

--- Chapter2/src/Triangle.java
+++ Chapter2/src/Triangle.java
@@ -1,10 +1,36 @@
 /**
  * Created by vld62 on 2/12/17.
  */
+import java.util.Scanner;
+
 public class Triangle {
-    public static void main (String [] args){
-        /*Напишете програма, която принтира на конзолата равнобедрен
-триъгълник, като страните му са очертани от символа звездичка "*".*/
-        System.out.println("*");
+    public static void main (String [] args) {
+         //Simple
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        //More complicated
+        int i, space, rows, k=0;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number of Rows : ");
+        rows = scan.nextInt();
+        for(i=1; i<=rows; i++)
+        {
+            for(space=1; space<=(rows-i); space++)
+            {
+                System.out.print("  ");
+            }
+            while(k != (2*i-1))
+            {
+                System.out.print("* ");
+                k++;
+            }
+            k = 0;
+            System.out.println();
+        }
     }
-}
+    }
+
+
