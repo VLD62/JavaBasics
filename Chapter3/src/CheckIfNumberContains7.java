15a73de492bc0a1eda30a6133afb7f221be79b5a
VLD62 <iliev62@gmail.com>
2017-02-12 20:22:57 +0200

    Adding excersices from Chapter 3

--- Chapter3/src/CheckIfNumberContains7.java
+++ Chapter3/src/CheckIfNumberContains7.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 /**
  * Created by vld62 on 2/12/17.
  */
@@ -7,14 +6,14 @@ public class CheckIfNumberContains7 {
     public static void main (String [] args){
         //Input number
         int num1;
-        int digit3;
+        String digit3;
         Scanner in= new Scanner(System.in);
         //Input number from console
         System.out.println("Enter number: ");
         num1 = in.nextInt();
-        digit3 = String(num1+"a").charAt(3);
+        digit3 = Integer.toString(num1);
         System.out.println("Num2 is :" + digit3);
-        if ( digit3 == 7) {
+        if ( Character.toString(digit3.charAt(2)).equals("7")) {
             System.out.println("Number contains 7 as 3rd digit");
         } else {
             System.out.println("Number does not contain 7 as 3rd digit");
