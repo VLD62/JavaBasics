32f863142be806f63bbf8e18f3c5896e5c0aab91
vld62 <iliev62@gmail.com>
2017-02-13 11:25:10 +0200

    adding changes to excersies in Chapter 3

--- Chapter2/src/ObjectType.java
+++ Chapter2/src/ObjectType.java
@@ -4,7 +4,7 @@
 public class ObjectType {
     public static void main (String args []) {
         //Declare variables
-        Object container = 5;
+        Object container = "5";
         Object container2 = "Five";
         //Print result to the console
         System.out.println("The value of container is " + container);
