0f412bb75ce111c382889782f93b8909cb5f187d
vld62 <iliev62@gmail.com>
2017-02-13 12:34:33 +0200

    adding changes to excersies in Chapter 3

--- Chapter3/src/CheckPointCircleRectangle.java
+++ Chapter3/src/CheckPointCircleRectangle.java
@@ -15,9 +15,20 @@ public class CheckPointCircleRectangle {
             System.out.println("Point is inside the circle K.");
         } else {
             System.out.println("Point is outside the circle K.");
+        }
+        //Check for the point if it is inside the circle and rectangle
+            boolean firstCase = Math.sqrt(x * x + y * y) <= 5; // First Condition
+            boolean secondCase = ((x > -1 || x < 1 || y < 5) && y > 0) ; // Second Condition
 
+            boolean resultThree = firstCase && secondCase;
+            if (resultThree) {
+                System.out.println("Point is inside the circle K and rectangular");
+            } else {
+                System.out.println("Point is outside the circle K and rectangular");
+              }
         }
 
-
     }
-}
+
+
+
