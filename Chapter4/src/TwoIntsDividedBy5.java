0d87669d20132ada09423ec34a3d66797f8e61d0
VLD62 <iliev62@gmail.com>
2017-02-13 23:34:02 +0200

    Adding exercises from Chapter4.

--- Chapter4/src/TwoIntsDividedBy5.java
+++ Chapter4/src/TwoIntsDividedBy5.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class TwoIntsDividedBy5 {
     public static void main (String [] args) {
         int a,b;
+        int counter = 1;
         Scanner in = new Scanner(System.in);
         System.out.println("Enter number 1: ");
         a = in.nextInt();
@@ -13,10 +14,11 @@ public class TwoIntsDividedBy5 {
         b = in.nextInt();
         for (int i = a; i <= b; i++){
             if (i % 5 == 0) {
-                System.out.print(i + " " + "\n");
+                counter+= 1;
+                System.out.println(i + " ");
             }
 
         }
-
+        System.out.println("Total of  " + counter + " occurrences.");
     }
 }
