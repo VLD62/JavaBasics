8da0b4ae77bcbc98812ea5f6c9de37a15e466653
vld62 <iliev62@gmail.com>
2017-02-15 12:30:58 +0200

    adding changes to excersies in Chapter 3

---
+++ Chapter5/src/CheckSignOfQuotient.java
@@ -0,0 +1,29 @@
+/**
+ * Created by viliev on 15.2.2017 г..
+ */
+import java.util.Scanner;
+
+public class CheckSignOfQuotient {
+    public static void main(String[] args) {
+    int a, b;
+    Scanner in = new Scanner(System.in);
+    System.out.println("Input first number: ");
+    a =in.nextInt();
+    System.out.println("Input first number: ");
+    b =in.nextInt();
+
+    if (a>0 && b >0) {
+        System.out.println("Quotient is positive.");
+    }
+    else if (a < 0 && b > 0){
+        System.out.println("Quotient is negative.");
+    }
+    else if (a > 0 && b < 0) {
+            System.out.println("Quotient is negative.");
+    }
+    else if (a < 0 && b > 0) {
+          System.out.println("Quotient is positive.");
+    }
+
+    }
+}
