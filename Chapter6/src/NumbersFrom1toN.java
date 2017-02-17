1dffa1e665ed92c2209b86dc5d006577fab0caa2
vld62 <iliev62@gmail.com>
2017-02-17 19:57:39 +0200

    Adding exercises from Chapter6

--- Chapter6/src/NumbersFrom1toN.java
+++ Chapter6/src/NumbersFrom1toN.java
@@ -4,14 +4,32 @@ import java.util.Scanner;
  * Created by vld62 on 2/16/17.
  */
 public class NumbersFrom1toN {
-    public static void main (String [] args){
-        int n;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please enter number: ");
-        n = in.nextInt();
-        for (int i=1;i<=n;i++){
-            System.out.println(i);
+    public static void main(String[] args) {
+        int s = 0, l = 0;
+        Scanner input = new Scanner(System.in);
+        //Read user  input.
+        System.out.println("Enter total count of all numbers: ");
+        int num = input.nextInt();
+
+        System.out.println("Enter numbers :");
+        for (int i = 0; i < num; i++) {
+            int x = input.nextInt();
+            if (i == 0) {
+                s = x;
+                l = x;
+            }
+            if (x < s) {
+                s = x;
+            }
+            if (x > l) {
+                l = x;
+            }
+
         }
+        System.out.println("\n" + "Smallest number is: " + s);
+        System.out.println("\n" + "Biggest number is: " + l);
     }
 }
+
+
 
