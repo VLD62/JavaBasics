56b96af6297dbad0c88cb11f8510993c53bb401c
VLD62 <iliev62@gmail.com>
2017-02-18 23:54:47 +0200

    Adding exercises from Chapter6.

---
+++ Chapter6/src/DecimalToBinaryConverter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by vld62 on 2/18/17.
+ */
+public class DecimalToBinaryConverter {
+    public static void main (String [] args){
+        int N;
+        Scanner in = new Scanner(System.in);
+
+    }
+}
