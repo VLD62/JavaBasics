eed164014603dffc91a8a4fae4991c4083ba8a33
vld62 <iliev62@gmail.com>
2017-02-20 18:50:45 +0200

    Adding exercises from Chapter7

--- Chapter7/src/Elemenents20Array.java
+++ Chapter7/src/Elemenents20Array.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  * на елемента умножен по 5. Елементите на масива да се изведат на конзолата.
  *
  */
-public class Elemenths20Array {
+public class Elemenents20Array {
     public static void main (String [] args){
         int Array [] = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
         for (int i = 0 ; i < Array.length; i++) {
