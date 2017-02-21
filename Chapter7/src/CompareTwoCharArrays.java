bce2b6cee335661f7810ec251baef50ba4dcdc3b
vld62 <iliev62@gmail.com>
2017-02-21 19:22:32 +0200

    Adding exercises from Chapter7

--- Chapter7/src/CompareTwoCharArrays.java
+++ Chapter7/src/CompareTwoCharArrays.java
@@ -8,43 +8,33 @@ import java.util.Scanner;
  */
 public class CompareTwoCharArrays {
     public static void main (String [] Args){
-        int N,M;
+        String N,M,K,L;
         Scanner in = new Scanner(System.in);
         System.out.println("Enter maximum number of strings in array 1: ");
-        N = in.nextInt();
-        String[] Array1 = new String[N];
+        N = in.nextLine();
+        String[] Array1 = new String[Integer.parseInt(N)];
         for (int i = 0 ; i < Array1.length; i++) {
             System.out.println("Enter " + i + " string in array 1: ");
             Array1[i] = in.nextLine();
         }
-        System.out.println("Elements of Array are: " + Arrays.toString(Array1));
+        System.out.println("Elements of Array 1 are: " + Arrays.toString(Array1));
         System.out.println("Enter maximum number of strings in Array 2: ");
-        M = in.nextInt();
-        String[] Array2 = new String[M];
+        M = in.nextLine();
+        String[] Array2 = new String[Integer.parseInt(M)];
         for (int i = 0 ; i < Array2.length; i++) {
             System.out.println("Enter " + i + " number in Array 2: ");
             Array2[i] = in.nextLine();
         }
-        System.out.println("Elements of Array are: " + Arrays.toString(Array2));
-        /*System.out.println("Comparing two arrays...");
-        if (Array1.length == Array2.length){
-            System.out.println("Arrays have same length.. checking elements..");
-            for (int i = 0; i < N; i++){
-                if (Array1[i] == Array2[i]){
-                    System.out.println("Number " + i + " is equal in both Arrays.");
-                    } else {
-                    System.out.println("Number " + i + " is not equal in both Arrays.");
-                    check = 1;
-                }
-            }
-            if (check == 0) {
-                System.out.println("Arrays have equal elements!");
-            } else {
-                System.out.println("Arrays have different elements!");
-            }
+        System.out.println("Elements of Array 2are: " + Arrays.toString(Array2));
+        K = Character.toString(Arrays.toString(Array1).charAt(1));
+        L = Character.toString(Arrays.toString(Array2).charAt(1));
+        if (K.compareTo(L) >= 0)
+        {
+            System.out.println("Array 1 is lexigrapically bigger or equal to Array2");// testArray[0] is lexigraphically "bigger" or equal
         } else {
-            System.out.println("Arrays have different length!");
-        }*/
+            System.out.println("Array 1 is lexigrapically smaller than Array2");// testArray[1] is lexigraphically "bigger"
+        }
+
 
 
     }
