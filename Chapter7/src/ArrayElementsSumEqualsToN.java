6f0940d0b39b1a8eb7f3c291b9e0bcda807314aa
vld62 <iliev62@gmail.com>
2017-02-24 15:09:53 +0200

    Adding exercises from Chapter7

--- Chapter7/src/ArrayElementsSumEqualsToN.java
+++ Chapter7/src/ArrayElementsSumEqualsToN.java
@@ -22,5 +22,30 @@ public class ArrayElementsSumEqualsToN {
         System.out.println("Elements of Array are: " + Arrays.toString(Array));
         System.out.println("Enter number S: ");
         S = in.nextInt();
+        ///
+        int sum = 0, start = 0, end =0;
+        boolean sumFound = false;
+        for (int i = 0; i < Array.length - 1; i++)
+        {
+            sum = Array[i];
+
+            for (int j = i + 1; j < Array.length; j++)
+            {
+                sum += Array[j];
+                if (sum == S)
+                {
+                    start = i;
+                    end = j;
+                    sumFound = true;
+                    break;
+                }
+            }
+
+            if (sumFound) break;
+        }
+
+        if (sumFound) for (int i = start; i <= end; i++) System.out.print(" " + Array[i]);
+        else System.out.println("No sum found.");
+        ///
     }
 }
