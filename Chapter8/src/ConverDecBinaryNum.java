b423d4dab6ac6eb37d6e8b2fa72d2edfd187aa8a
vld62 <iliev62@gmail.com>
2017-03-02 18:53:16 +0200

    Adding exercises from Chapter8

---
+++ Chapter8/src/ConverDecBinaryNum.java
@@ -0,0 +1,14 @@
+/**
+ * Created by viliev on 2.3.2017 г..
+ * Да се напише програма, която преобразува десетично число в двоично.
+ */
+import java.util.Scanner;
+public class ConverDecBinaryNum {
+    public static void main (String [] Args){
+        int N;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter number: ");
+        N = in.nextInt();
+        System.out.println("Number " + N + " converted in binary is: " + Integer.toBinaryString(N));
+    }
+}
