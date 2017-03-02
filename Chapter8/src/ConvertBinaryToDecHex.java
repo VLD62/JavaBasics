fd86d6754db782c0422960f2a51ef5ce2cc6813d
vld62 <iliev62@gmail.com>
2017-03-02 14:36:29 +0200

    Adding exercises from Chapter8

---
+++ Chapter8/src/ConvertBinaryToDecHex.java
@@ -0,0 +1,10 @@
+/**
+ * Created by viliev on 2.3.2017 г.
+ * Превърнете числото 1111010110011110(2)
+ * в шестнадесетична и десетична бройна система.
+ */
+public class ConvertBinaryToDecHex {
+    public static void main (String [] Args){
+
+    }
+}
