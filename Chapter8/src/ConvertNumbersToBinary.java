4d67247a949a3304d36d764139c73eec16d853b5
vld62 <iliev62@gmail.com>
2017-03-02 14:28:20 +0200

    Adding exercises from Chapter8

--- Chapter8/src/ConvertNumbersToBinary.java
+++ Chapter8/src/ConvertNumbersToBinary.java
@@ -1,6 +1,6 @@
 /**
  * Created by viliev on 2.3.2017 г..
- Превърнете числата 151, 35, 43, 251 и -0,41 в двоична бройна система.
+ * Превърнете числата 151, 35, 43, 251 и -0,41 в двоична бройна система.
  */
 public class ConvertNumbersToBinary {
     public static void main (String [] Args){
@@ -8,6 +8,12 @@ public class ConvertNumbersToBinary {
         System.out.println("Binary eqivalent of 35 = " + Integer.toString(35, 2));
         System.out.println("Binary eqivalent of 43 = " + Integer.toString(43, 2));
         System.out.println("Binary eqivalent of 251 = " + Integer.toString(251, 2));
-      //  System.out.println("Binary eqivalent of -0,41 = " + double.toString(-0,41, 2));
+        double d = -0.41;
+        System.out.println("Binary eqivalent of -0,41 = " +Long.toBinaryString(Double.doubleToRawLongBits(d)));
+
+
     }
+
+
+
 }
