d154738e12fbcc9f542be8a70b606704f0935303
VLD62 <iliev62@gmail.com>
2017-03-10 00:20:07 +0200

    Adding exercises from Chapter8.

--- Chapter9/src/CheckElementInArray.java
+++ Chapter9/src/CheckElementInArray.java
@@ -9,11 +9,47 @@ import java.util.Scanner;
  */
 public class CheckElementInArray {
 
+    static void CheckArrayElement(int position, int[] arr)
+    {
+        if (position == 0)
+        {
+            if (arr[0] < arr[1]) System.out.printf("%d is smaller than it's right.", arr[0]);
+            else if (arr[0] > arr[1]) System.out.printf("%d is bigger than it's right.", arr[0]);
+            else System.out.printf("%d is equal to it's right.", arr[0]);
+        }
+        else if (position == arr.length - 1)
+        {
+            if (arr[arr.length - 1] < arr[arr.length - 2]) System.out.printf("%d is smaller than it's left.", arr[position]);
+            else if (arr[arr.length - 1] > arr[arr.length - 2]) System.out.printf("%d is bigger than it's left.", arr[position]);
+            else System.out.printf("%d is equal to it's left.", arr[position]);
+        }
+        else
+        {
+            if (arr[position] < arr[position - 1])
+            {
+                if (arr[position] < arr[position + 1]) System.out.printf("%d is smaller than it's neighbours.", arr[position]);
+                else if (arr[position] == arr[position + 1]) System.out.printf("%d is smaller than it's left and equal to it's right.", arr[position]);
+                else System.out.printf("%d is smaller than it's left and bigger than it's right.", arr[position]);
+            }
+            else if (arr[position] == arr[position - 1])
+            {
+                if (arr[position] < arr[position + 1]) System.out.printf("%d is euqal to it's left and smaller than it's right.", arr[position]);
+                else if (arr[position] == arr[position + 1]) System.out.printf("%d is equal to it's neighbours.", arr[position]);
+                else System.out.printf("%d is equal to it's left and bigger than it's right.", arr[position]);
+            }
+            else
+            {
+                if (arr[position] < arr[position + 1]) System.out.printf("%d is bigger than it's left and smaller than it's right.", arr[position]);
+                else if (arr[position] == arr[position + 1]) System.out.printf("%d is bigger than it's left and equal to it's right.", arr[position]);
+                else System.out.printf("%d is bigger than it's neighbours.", arr[position]);
+            }
+        }
+    }
+
     public static void main (String [] Args ){
         int n,x,y;
         Scanner in = new Scanner(System.in);
-        System.out.println("Please insert number: ");
-        n = in.nextInt();
+
         System.out.println("Please insert array length: ");
         x = in.nextInt();
         int [] Array = new int[x];
@@ -22,7 +58,10 @@ public class CheckElementInArray {
             y = in.nextInt();
             Array[i] = y;
         }
+        System.out.println("Please insert number for a position in the array: ");
+        n = in.nextInt();
         System.out.println("Entered array is: " + Arrays.toString(Array));
+        CheckArrayElement(n,Array);
     }
 
 }
