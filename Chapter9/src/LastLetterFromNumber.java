5b79e3dfd96235347ef94c7b6ec3f6159b17138a
vld62 <iliev62@gmail.com>
2017-03-09 17:00:24 +0200

    Adding exercises from Chapter9

---
+++ Chapter9/src/LastLetterFromNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by viliev on 9.3.2017 г..
+ */
+public class LastLetterFromNumber {
+    public static void main (String [] Args){
+        int number;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter number: ");
+    }
+}
