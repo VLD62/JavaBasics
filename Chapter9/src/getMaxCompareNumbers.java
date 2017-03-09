9d1d22fcfd9ebcd82eee4418ba7ba1a8c3184ca1
vld62 <iliev62@gmail.com>
2017-03-09 16:25:06 +0200

    Adding exercises from Chapter8

---
+++ Chapter9/src/getMaxCompareNumbers.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**
+ * Created by viliev on 9.3.2017 г..
+ */
+public class getMaxCompareNumbers {
+
+    public static void getMax ( int x, int y) {
+        if (x > y){
+            System.out.println("Bigger number is : " + x);
+        }
+        else if (x == y) {
+            System.out.println("All numbers are equal to: " + x);
+        }
+        else {
+            System.out.println("Biggest number is : " + y);
+        }
+    }
+
+    public static void main (String [] args){
+        int a,b,c,d;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter first number: ");
+        a = in.nextInt();
+        System.out.println("Please enter second number: ");
+        b = in.nextInt();
+        System.out.println("Please enter third number: ");
+        c = in.nextInt();
+
+        if ( a > b) {
+            d = a;
+        }
+
+        else if (a == b) {
+            d = a;
+        }
+
+        else {
+            d = b;
+        }
+        getMax(c , d);
+
+
+    }
+
+}
