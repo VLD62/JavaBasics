885a263a1837388a14a46d7eb273f75742b5cefc
vld62 <iliev62@gmail.com>
2017-03-24 18:57:02 +0200

    Adding exercises from Chapter12

---
+++ Chapter12/src/AllStandardExceptionsFromIOExceptions.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+/**
+ * Created by viliev on 24.3.2017 г..
+ * Да се намерят всички стандартни изключения от йерархията на java.io.IOException.
+ */
+public class AllStandardExceptionsFromIOExceptions {
+}
