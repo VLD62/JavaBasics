ca8bc999a4d3413fbfd2bacd0c726b5c2f9bb09c
VLD62 <iliev62@gmail.com>
2017-04-09 19:45:18 +0300

    Adding exercises from Chapter13

---
+++ Chapter13/src/ChangeLettersCaseAroundTag.java
@@ -0,0 +1,13 @@
+/**
+ * Created by vld62 on 4/9/17.
+ * Даден е текст. Напишете програма, която променя регистъра на буквите
+ на всички места в текста, заградени с таговете <upcase> и </upcase>.
+ Таговете не могат да бъдат вложени.
+ Пример:
+ We are living in a <upcase>yellow submarine</upcase>. We don't
+ have <upcase>anything</upcase> else.
+ Резултат:
+ We are living in a YELLOW SUBMARINE. We don't have ANYTHING else.
+ */
+public class ChangeLettersCaseAroundTag {
+}
