461f5a97d291a6716099429fbdf55ebc6aa95f95
vld62 <iliev62@gmail.com>
2017-04-11 18:36:48 +0300

    Adding exercises from Chapter13

--- Chapter13/src/ChangeLettersCaseAroundTag.java
+++ Chapter13/src/ChangeLettersCaseAroundTag.java
@@ -12,11 +12,14 @@
 public class ChangeLettersCaseAroundTag {
 
     public static void changeCase (String text) {
-
+     int index1 = text.indexOf("<upcase>");
+     int index2 = text.indexOf("</upcase>");
+     String text3 = text.substring(index1+8,index2).toUpperCase();
+     System.out.println("Index1 is: " + index1 + " Index2 is: " + index2 + " Text is: " + text3);
     }
 
     public static void main (String [] args) {
         String text = "We are living in a <upcase>yellow submarine</upcase>. We don't have <upcase>anything</upcase> else.";
-
+        changeCase(text);
     }
 }
