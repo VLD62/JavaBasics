b7d662e24b2a103b8a719230fec64e6943ad89a2
vld62 <iliev62@gmail.com>
2017-04-11 20:48:40 +0300

    Adding exercises from Chapter13

--- Chapter13/src/ChangeLettersCaseAroundTag.java
+++ Chapter13/src/ChangeLettersCaseAroundTag.java
@@ -10,16 +10,42 @@
  We are living in a YELLOW SUBMARINE. We don't have ANYTHING else.
  */
 public class ChangeLettersCaseAroundTag {
-
     public static void changeCase (String text) {
+
      int index1 = text.indexOf("<upcase>");
      int index2 = text.indexOf("</upcase>");
-     String text3 = text.substring(index1+8,index2).toUpperCase();
-     System.out.println("Index1 is: " + index1 + " Index2 is: " + index2 + " Text is: " + text3);
+
+     StringBuilder sb = new StringBuilder();
+
+     /*String[] splittedText = text.split("upcase");
+
+     for (int indx =0 ; indx < splittedText.length ; indx ++){
+         sb.append(splittedText[indx]);
+         System.out.println(splittedText[indx]);
+
+     }*/
+     while (index1 != -1 && index2 != -1 ) {
+         System.out.println("Index1 is: " + index1 + " Index2 is: " + index2 + " Text is: " );// + text3);
+
+
+        String text3 = text.substring(index1 + 8, index2).toUpperCase();
+        String newText = text.replace(text.substring(index1, index2 + 9), text3);
+
+        System.out.println("Replaced text is :" + newText);
+
+        index1 = text.indexOf("<upcase>", index1+1);
+        index2 = text.indexOf("</upcase>", index2+1);
+
+        sb.append(newText);
+
+
+
+     }
+     System.out.println("Final text is: " + sb);
     }
 
     public static void main (String [] args) {
-        String text = "We are living in a <upcase>yellow submarine</upcase>. We don't have <upcase>anything</upcase> else.";
+        String text = "We are living in a <upcase>yellow submarine</upcase>. We don't have <upcase>anything</upcase> else. Kozata e nai <upcase>moshnata</upcase> koza. ";
         changeCase(text);
     }
 }
--- Chapter13/src/FindSubstringInText.java
+++ Chapter13/src/FindSubstringInText.java
@@ -12,8 +12,7 @@ import java.util.Scanner;
 public class FindSubstringInText {
     public static void substringFinder (String text, String substring) {
         int counter = 0;
-        for (int n = text.toLowerCase().indexOf(substring.toLowerCase()); n >=0;
-             n = text.toLowerCase().indexOf(substring.toLowerCase(), n+1)) {
+        for (int n = text.toLowerCase().indexOf(substring.toLowerCase()); n >=0; n = text.toLowerCase().indexOf(substring.toLowerCase(), n+1)) {
            counter++;
         }
         System.out.println("Substring " + substring + " is found " + counter + " times in the text");
